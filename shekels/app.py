48c1d0afba84794c9b8ddbfffb9447bfec30aee2
marstom <marstomek@gmail.com>
2017-10-31 12:20:56 +0000

    This is version for py_anywhere

--- shekels/app.py
+++ shekels/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
-from flask_debugtoolbar import DebugToolbarExtension
+#from flask_debugtoolbar import DebugToolbarExtension
 from flask_migrate import Migrate
 import shekels.logger
 from shekels.db_sqlalchemy import db
@@ -8,11 +8,11 @@ from shekels.db_sqlalchemy import db
 
 shekels.logger.setup()
 
-app = Flask('shekels')
+app = Flask(__name__)
 app.secret_key = 'fdsafhsdalkghsdahg'
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://marstom_shekels:marstom_shekels@188.40.77.144/marstom_shekels'
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://marstom_shekels:marstom_shekels@188.40.77.144/marstom_shekels'
+#app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login_manager = LoginManager()
@@ -20,7 +20,7 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message = "Log in please!"
 
-toolbar = DebugToolbarExtension(app)
+#toolbar = DebugToolbarExtension(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
